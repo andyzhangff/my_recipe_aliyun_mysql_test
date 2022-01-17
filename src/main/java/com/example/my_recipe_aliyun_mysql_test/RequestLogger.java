b98b08304fb326ddfe8ffd93e2736c5f65c231f8
andyzhangff <Zf@200336>
2022-01-17 23:41:58 +0800

    try to deploy to aws

---
+++ src/main/java/com/example/my_recipe_aliyun_mysql_test/RequestLogger.java
@@ -0,0 +1,20 @@
+package com.example.my_recipe_aliyun_mysql_test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLogger {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(false);
+        return loggingFilter;
+    }
+
+}
